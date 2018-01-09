c323ee2eb48d98ea7d11d291dfce25c81586b173
Lawrence Caulker <locaulker@gmail.com>
2018-01-09 16:45:45 -0500

    setting up mysql db and ORM completed

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -7,6 +7,9 @@ if(!isset($_SESSION)) session_start();
 /** Load Environment Variables */
 require_once __DIR__ . './../app/config/_env.php';
 
+// Instantiate database class
+new \App\Classes\Database();
+
 require_once __DIR__ . '/../app/routing/routes.php';
 
 new \App\RouteDispatcher($router);
--- public/index.php
+++ public/index.php
@@ -5,8 +5,9 @@ require_once __DIR__ . '/../bootstrap/init.php';
 
 $app_name = getenv('APP_NAME');
 
-// echo $app_name . '<br>';
+use Illuminate\Database\Capsule\Manager as Capsule;
 
-// This will only work if apache is run as a module
-// var_dump(in_array('mod_rewrite', apache_get_modules()));
+$category = Capsule::table('categories')->get();
 
+echo '<pre>';
+var_dump($category->toArray());
