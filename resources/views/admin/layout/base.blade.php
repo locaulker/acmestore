bd31cc8b830c6e4fb9c70443eca47a7ee81feedf
Lawrence Caulker <locaulker@gmail.com>
2018-01-11 11:00:02 -0500

    admin section sidebar menu styles applied

--- resources/views/admin/layout/base.blade.php
+++ resources/views/admin/layout/base.blade.php
@@ -5,27 +5,18 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Admin Panel - @yield('title')</title>
+
   <link rel="stylesheet" href="/css/bundle.css">
+  <script src="https://use.fontawesome.com/702e01aec3.js"></script>
 </head>
 <body>
   
-  <div class="off-canvas position-left reveal-for-large" id="offCanvas" data-off-canvas>
-    <!-- Sidebar Menu starts -->
-    <ul class="vertical menu">
-      <li><a href="#">Foundation</a></li>
-      <li><a href="#">Dot</a></li>
-      <li><a href="#">ZURB</a></li>
-      <li><a href="#">Com</a></li>
-      <li><a href="#">Slash</a></li>
-      <li><a href="#">Sites</a></li>
-    </ul>
-    <!-- Sidebar Menu ends -->
-  </div>
+  @include('includes.admin-sidebar')
 
-  <div class="off-canvas-content" data-off-canvas-content>
+  <div class="off-canvas-content admin-title-bar" data-off-canvas-content>
     <!-- Your page content lives here -->
 
-  <!-- Top Navbar -->
+  <!-- Top title brand bar -->
   <div class="title-bar">
     <div class="title-bar-left">
       <button class="menu-icon hide-for-large" type="button" data-open="offCanvas"></button>
